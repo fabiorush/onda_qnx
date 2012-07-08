da6e5230664b9ae71012cbc58063bf684c77157c
U-CESAR\flus <flus@IMBASSUABA.cesar.intranet>
2012-07-07 22:39:06 -0300

    Addicionado app client cgi.

---
+++ client/client.c
@@ -0,0 +1,98 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <sys/iofunc.h>
+#include <sys/dispatch.h>
+
+/* We specify the header as being at least a pulse */
+typedef struct _pulse msg_header_t;
+
+typedef struct _my_data {
+    msg_header_t hdr;
+    int data;
+} my_data_t;
+
+int find_param(char *query_str, char *substr, int *ret_value) {
+	int found = 0, value = 0;
+	char *fs = strstr(query_str, substr);
+	if (fs) {
+		fs += strlen(substr);
+
+		while (*fs <= '9' && *fs >= '0') {
+			found = 1;
+			value = value * 10 + *fs++ - '0';
+		}
+
+		if (found && ret_value) {
+			*ret_value = value;
+		}
+	}
+	return found;
+}
+
+int main(int argc, char *argv[]) {
+	char query_str[1024];
+    my_data_t msg;
+    int value;
+    int server_coid;
+    size_t len;
+
+    printf(	"Content-Type: text/html\n\n"
+    		"<html><head><title>Teste</title></head>\n"
+    		"<body><h1>Teste</h1>\n");
+
+#if 0
+    clock_t future_clock, current_clock = clock();
+    future_clock = CLOCKS_PER_SEC * 10 + current_clock;
+
+    while (current_clock < future_clock) {
+    	current_clock = clock();
+    }
+
+	printf("Done!\n");
+#endif
+
+	if ((server_coid = name_open("onda", NAME_FLAG_ATTACH_GLOBAL)) == -1) {
+		printf("Falha na conexão com onda app</body></html>\n");
+	    return EXIT_SUCCESS;
+	}
+
+	len = fread(query_str, 1, 1023, stdin);
+    if (len) {
+    	find_param(query_str, "interval=", &msg.data);
+    	if (find_param(query_str, "interval=", &msg.data)) {
+   			msg.hdr.type = 0x00;
+   			msg.hdr.subtype = 0x66;
+
+			/* set onda interval */
+			MsgSend(server_coid, &msg, sizeof(msg), &value, sizeof(value));
+    	}
+    	if (find_param(query_str, "cpuload=", NULL)) {
+    		/* start cpuload thread */
+    	}
+    }
+
+	msg.hdr.type = 0x00;
+	msg.hdr.subtype = 0x55;
+
+	if (MsgSend(server_coid, &msg, sizeof(msg), &value, sizeof(value)) != -1) {
+		printf("Botao pressionado %d vezes\n", value);
+	}
+
+	msg.hdr.type = 0x00;
+	msg.hdr.subtype = 0x65;
+
+	if (MsgSend(server_coid, &msg, sizeof(msg), &value, sizeof(value)) != -1) {
+		printf(	"<form name=\"input\" action=\"client\" method=\"post\">\n"
+	    		"Onda interval: <input type=\"text\" name=\"interval\" value=\"%d\"/><br>\n"
+				"<input type=\"checkbox\" name=\"cpuload\" value=\"1\" />Load CPU<br>\n"
+	    		"<input type=\"submit\" value=\"Submit\" /></form>", value);
+	}
+
+	printf("</body></html>\n");
+
+	/* Close the connection */
+	name_close(server_coid);
+    return EXIT_SUCCESS;
+}
